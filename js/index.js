c3ba58600f49318747615c55850d04b7e2a406f1
Anton-Sakharov-hub <saharovanton357@gmail.com>
2021-09-21 13:45:53 +0300

    создает функцию delay, которая возвращает promise через ms

---
+++ js/index.js
@@ -0,0 +1,22 @@
+
+// создаем функцию delay, которая возвращает промис
+// нужно создать промис, который перейдет в состояние resolved в этой функции
+// промис переходит в resolved через ***мс, которые передали в параметр функции
+
+
+function delay(ms) {
+    const time = new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(ms);
+        }, ms)
+    });
+
+    return time;
+};
+
+const logger = time => console.log(`Resolved after ${time}ms`);
+
+
+delay(2000).then(logger); // Resolved after 2000ms
+delay(1000).then(logger); // Resolved after 1000ms
+delay(1500).then(logger); // Resolved after 1500ms
